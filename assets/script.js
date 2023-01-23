3e6ad2c192137ed227bb381d9784cadbf9a7cc5a
Gini Stanzi <a.stanzione14@gmail.com>
2023-01-23 09:10:38 +0000

    Updated button function to save the entered value to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
-
+$("textarea").val(localStorage.getItem("9a"));
 //Set current date and time 
 setInterval(
 
     function(){
         let $currentDay = $("#currentDay")
         $("#currentDay").text(moment().format("MMM Do, YYYY, HH:mm:ss"));
-},1000)
-;
+},1000);
 
+
+$("button").on("click", function(){
+   
+let $taskEntered = $("textarea").val()
+localStorage.setItem("9a", $taskEntered);
+
+});
 
 
 
