f5e83106e7f17318f4fe9261ac1b19e77c9223df
Gini Stanzi <a.stanzione14@gmail.com>
2023-01-24 21:53:50 +0000

    added further time blocks with correct color settings finalized and set local storage for first time block

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// $("textarea").val(localStorage.getItem("9"));
+
 //Set current date and time 
 setInterval(
 
@@ -12,28 +12,20 @@ $("button").on("click", function(e){
 console.log($(e))
 let $taskEntered = $("textarea").val()
 localStorage.setItem("9", $taskEntered);
-
 });
 
-// let timeBlockArray = [];
-
-// $("textarea").each(function(){
-
-//     timeBlockArray.push($(this).attr("data-time"));
-    
-
-// }
-// );
-// console.log(timeBlockArray)
 
 
 //Setting time block colors based on current time
+
 let $timeBlock = $(".time-block")
-//$(".time-block").each(function(){
-//$timeBlock.forEach(element => {
+
 for(i=0; i<$timeBlock.length; i++) {  
+   
     let localStorageReference = $($timeBlock[i]).attr("data-time") 
     $($timeBlock[i]).val(localStorage.getItem(localStorageReference))
+
+    
 if (parseInt($($timeBlock[i]).attr("data-time")) < parseInt(moment().format("HH"))){
 
     $($timeBlock[i]).addClass("past")
